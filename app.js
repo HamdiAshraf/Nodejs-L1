db33e2be7873bb5e1c7ea5ae45a8b4f8cbd3d8ec
HamdiAshraf <hamdiashraf2000@gmail.com>
2024-04-04 01:20:32 +0300

    connect project to database

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+
+
 
 app.get('/', (req, res) => {
     res.sendFile('./views/homePage.html', { root: __dirname });
 })
 
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
-})
+
+
+mongoose.connect('mongodb+srv://hamdiashraf:3pyQBRV5atlV6XS1@cluster0.rempz7k.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+    })
+
+}).catch((err) => console.log(err))
